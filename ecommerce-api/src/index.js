db5dafb1c96f0939e5d434dac3df680efe23f42a
Rafael <r.3.c023@gmail.com>
2025-07-26 09:56:05 -0500

    feat (router): Implementación de rutas y pruebas del CRUD de products
    Se implementan las rutas correspondientes al CRUD de productos.
    
    Se prueban los diferentes endpoints utilizando Postman.

--- ecommerce-api/src/index.js
+++ ecommerce-api/src/index.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const {routersCustomers} = require('./routers/customerRouters.js')
-
-
-
+const { routersCustomers } = require('./routers/customerRouters.js')
+const {routersProducts} = require('./routers/productRouters.js' )
 const app = express()
 const PORT = 3000
 
-
-
-app.use('/api/customers', routersCustomers)
 require('dotenv').config();
-
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log("Connection successful"))
     .catch((error) => console.log(error));
 
+app.use('/api/customers', routersCustomers)
+app.use('/api/products',routersProducts)
+
+
 
 app.get('/', (req, res) => {
-
     res.send('api ecommerce ')
-
-
 })
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port http://localhost:${PORT} ...`)
